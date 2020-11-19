6fa0423d29042ff0ea530154e19d9c4e1238c7db
montipallarols <montaga.pallarols@gmail.com>
2020-11-19 15:18:06 +0100

    Change App.js to flexbox styling

--- App.js
+++ App.js
@@ -1,75 +1,101 @@
-import React, { useRef, useState } from "react";
-import { Text, View, ActivityIndicator, Image, Button, Alert, FlatList, TouchableOpacity, TextInput } from "react-native";
-
-function MyHeader() {
-  return <View style={{ marginHorizontal: 40, marginVertical: 60 }}>
-  <Text style={{ fontWeight: "bold", fontSize: 24, marginBottom: 30 }}>
-    Hello React Native
-  </Text>
-  <ActivityIndicator
-    size="large"
-    color="#c1262c"
-    style={{ marginBottom: 30 }}
-  />
-    </View>
-}
-
-function MyFooter() {
-  const [value, setValue] = useState("");
-
-  return (
-  <View>
-  <View
-  style={{
-    borderWidth: 2,
-    borderColor: "black",
-    padding: 20,
-    marginBottom: 30
-  }}
-  >
-  <Text>Hello again!</Text>
-  </View>
-  <Button
-  onPress={() => Alert.alert(value)}
-  title="Learn More"
-  color="#c1262c"
-  />
-  <TextInput
-  style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-  onChangeText={text => setValue(text)}
-  value={value}
-  />
-  </View>
-  )
-  }
+import React from "react";
+import { View } from "react-native";
 
 export default function App() {
-  const randomNum = useRef(Math.random()).current;
-
   return (
-    
-      <FlatList
-      ListHeaderComponent={MyHeader}
-      data={[0, 1, 2, 3, 4]}
-      renderItem={({ item }) => {
-      return (
-      <TouchableOpacity
-        onPress={() => Alert.alert(`You pressed image #${item + 1}`)}
-      >
-        <Image
-          source={{
-            uri: `https://picsum.photos/500/300?random=${randomNum +
-              item}`
-          }}
-          style={{ width: "100%", height: 160, marginBottom: 30 }}
-        />
-      </TouchableOpacity>
-      );
-      }}
-      keyExtractor={item => String(item)}
-      ListFooterComponent={MyFooter}
-      
+    <View style={{ flexDirection: "column", justifyContent: "space-around", alignItems: "center", height: "100%" }}>
+      <View
+        style={{ width: "20%", height: "30%", backgroundColor: "rebeccapurple" }}
       />
+      <View style={{ width: "50%", height: "20%", backgroundColor: "crimson" }} />
+      <View style={{ width: "80%", height: "20%", backgroundColor: "gold" }} />
+    </View>
   );
 }
+
+
+
+// flexDirection, alignItems, justifyContent, alignSelf
+
+
+
+
+
+
+
+
+// import React, { useRef, useState } from "react";
+// import { Text, View, ActivityIndicator, Image, Button, Alert, FlatList, TouchableOpacity, TextInput } from "react-native";
+
+// function MyHeader() {
+//   return <View style={{ marginHorizontal: 40, marginVertical: 60 }}>
+//   <Text style={{ fontWeight: "bold", fontSize: 24, marginBottom: 30 }}>
+//     Hello React Native
+//   </Text>
+//   <ActivityIndicator
+//     size="large"
+//     color="#c1262c"
+//     style={{ marginBottom: 30 }}
+//   />
+//     </View>
+// }
+
+// function MyFooter() {
+//   const [value, setValue] = useState("");
+
+//   return (
+//   <View>
+//   <View
+//   style={{
+//     borderWidth: 2,
+//     borderColor: "black",
+//     padding: 20,
+//     marginBottom: 30
+//   }}
+//   >
+//   <Text>Hello again!</Text>
+//   </View>
+//   <Button
+//   onPress={() => Alert.alert(value)}
+//   title="Learn More"
+//   color="#c1262c"
+//   />
+//   <TextInput
+//   style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+//   onChangeText={text => setValue(text)}
+//   value={value}
+//   />
+//   </View>
+//   )
+//   }
+
+// export default function App() {
+//   const randomNum = useRef(Math.random()).current;
+
+//   return (
+    
+//       <FlatList
+//       ListHeaderComponent={MyHeader}
+//       data={[0, 1, 2, 3, 4]}
+//       renderItem={({ item }) => {
+//       return (
+//       <TouchableOpacity
+//         onPress={() => Alert.alert(`You pressed image #${item + 1}`)}
+//       >
+//         <Image
+//           source={{
+//             uri: `https://picsum.photos/500/300?random=${randomNum +
+//               item}`
+//           }}
+//           style={{ width: "100%", height: 160, marginBottom: 30 }}
+//         />
+//       </TouchableOpacity>
+//       );
+//       }}
+//       keyExtractor={item => String(item)}
+//       ListFooterComponent={MyFooter}
+      
+//       />
+//   );
+// }
 

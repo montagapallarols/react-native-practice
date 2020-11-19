87bada49e115a428633719ab366b3c15fc34eb79
montipallarols <montaga.pallarols@gmail.com>
2020-11-19 12:39:30 +0100

    Change from ScrollView to FlatList

--- App.js
+++ App.js
@@ -1,21 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef, useState } from "react";
+import { Text, View, ActivityIndicator, Image, Button, Alert, FlatList, TouchableOpacity, TextInput } from "react-native";
+
+function MyHeader() {
+  return <View style={{ marginHorizontal: 40, marginVertical: 60 }}>
+  <Text style={{ fontWeight: "bold", fontSize: 24, marginBottom: 30 }}>
+    Hello React Native
+  </Text>
+  <ActivityIndicator
+    size="large"
+    color="#c1262c"
+    style={{ marginBottom: 30 }}
+  />
+    </View>
+}
+
+function MyFooter() {
+  const [value, setValue] = useState("");
+
+  return (
+  <View>
+  <View
+  style={{
+    borderWidth: 2,
+    borderColor: "black",
+    padding: 20,
+    marginBottom: 30
+  }}
+  >
+  <Text>Hello again!</Text>
+  </View>
+  <Button
+  onPress={() => Alert.alert(value)}
+  title="Learn More"
+  color="#c1262c"
+  />
+  <TextInput
+  style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+  onChangeText={text => setValue(text)}
+  value={value}
+  />
+  </View>
+  )
+  }
 
 export default function App() {
+  const randomNum = useRef(Math.random()).current;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+      <FlatList
+      ListHeaderComponent={MyHeader}
+      data={[0, 1, 2, 3, 4]}
+      renderItem={({ item }) => {
+      return (
+      <TouchableOpacity
+        onPress={() => Alert.alert(`You pressed image #${item + 1}`)}
+      >
+        <Image
+          source={{
+            uri: `https://picsum.photos/500/300?random=${randomNum +
+              item}`
+          }}
+          style={{ width: "100%", height: 160, marginBottom: 30 }}
+        />
+      </TouchableOpacity>
+      );
+      }}
+      keyExtractor={item => String(item)}
+      ListFooterComponent={MyFooter}
+      
+      />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

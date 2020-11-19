6a5a14c50fdc2febd5a805fe971a68888f0d9d82
montipallarols <montaga.pallarols@gmail.com>
2020-11-19 16:30:53 +0100

    Set up navigator screens

--- App.js
+++ App.js
@@ -1,8 +1,21 @@
 import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import GameScreen from "./screens/GameScreen";
+import HomeScreen from "./components/HomeScreen";
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return <GameScreen />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Game" component={GameScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+ 
 }
 
 
